818ae627854f859af707eb60850ae77449ace0ae
scott wright <scotty.wright30@gmail.com>
2020-07-14 16:04:58 +0100

    changes for deplotment

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using API.Extensions;
 using API.Middleware;
 using AutoMapper;
@@ -8,6 +9,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using StackExchange.Redis;
 
 namespace API
@@ -65,6 +67,12 @@ namespace API
             app.UseRouting();
 
             app.UseStaticFiles();
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(
+                    Path.Combine(Directory.GetCurrentDirectory(), "Content")
+                ), RequestPath = "/content"
+            });
 
             app.UseCors("CorsPolicy");
 
@@ -76,6 +84,7 @@ namespace API
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapFallbackToController("Index", "Fallback");
             });
         }
     }
